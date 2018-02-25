4664a8ab4c26c3a1bac2da8c6b5805e3c2801b18
DenisBelobrotski <belobrotkiy@gmail.com>
2018-02-25 07:07:39 -0800

    Add view with Google Maps

--- Wimix-Test/ViewController.swift
+++ Wimix-Test/ViewController.swift
@@ -1,25 +1,46 @@
-//
-//  ViewController.swift
-//  Wimix-Test
-//
-//  Created by Denis Belobrotski on 22.02.2018.
-//  Copyright © 2018 Denis Belobrotski. All rights reserved.
-//
-
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    let defaultCameraZoom: Float = 17.0
+    let gmsApiKey = "AIzaSyBV0Y4BpcxlDGJpagnzc4PjDGFAZtu5eOY"
 
+    
+    // MARK: - UIViewController
+    
+    override func loadView() {
+        let locationManager = CLLocationManager()
+        
+        locationManager.requestWhenInUseAuthorization()
+
+        GMSServices.provideAPIKey(gmsApiKey)
+        GMSPlacesClient.provideAPIKey(gmsApiKey)
+        
+        var startCamera: GMSCameraPosition
+        
+        if let currentLocation = locationManager.location {
+            startCamera = GMSCameraPosition.camera(withLatitude: currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude, zoom: defaultCameraZoom)
+        } else {
+            startCamera = GMSCameraPosition.camera(withLatitude: 0, longitude: 0, zoom: defaultCameraZoom)
+        }
+        
+        let mapView = GMSMapView.map(withFrame: .zero, camera: startCamera)
+        
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        mapView.settings.compassButton = true
+        self.view = mapView
+        
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
